d6fc29c607ca9127d77106b12674fb95ec50e19a
rht-arch <rafaharotello2003@gmail.com>
2025-02-19 17:47:45 +0100

    Initial commit

---
+++ src/main/java/org/example/trabajograndao/TrabajoGranDaoApplication.java
@@ -0,0 +1,13 @@
+package org.example.trabajograndao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoGranDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoGranDaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/trabajograndao/TrabajoGranDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trabajograndao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoGranDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
