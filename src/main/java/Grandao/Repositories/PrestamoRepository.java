c05f883e26a1669d5e8c58dc51a308d241a3cf40
rht-arch <rafaharotello2003@gmail.com>
2025-02-23 17:04:12 +0100

    Repositorios restantes de JPA

---
+++ src/main/java/Grandao/Repositories/PrestamoRepository.java
@@ -0,0 +1,8 @@
+package Grandao.Repositories;
+
+import Grandao.DTO.Prestamo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrestamoRepository extends JpaRepository<Prestamo, Integer> {
+
+}
