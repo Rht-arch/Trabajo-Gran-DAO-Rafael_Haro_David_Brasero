6500b24881f70319f0f50f41f2942b3a35282718
rht-arch <rafaharotello2003@gmail.com>
2025-02-23 16:41:17 +0100

    Creación DTO

---
+++ src/main/java/Grandao/Repositories/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package Grandao.Repositories;
+
+import Grandao.DTO.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+}
